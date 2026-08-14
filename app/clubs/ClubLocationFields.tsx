4b8211728cf9f14f9da0aab47c8b44adb7a1e734
3K Sportz <shivkalola@gmail.com>
2026-08-14 11:30:58 -0400

    Align nationwide club location control

--- app/clubs/ClubLocationFields.tsx
+++ app/clubs/ClubLocationFields.tsx
@@ -8,27 +8,25 @@ export default function ClubLocationFields() {
   return (
     <div className="club-location-fields">
       <div className="club-form-grid">
-        <div className="club-location-city">
-          <label>
-            <span>City</span>
-            <input name="city" placeholder="Charlotte" required={!nationwide} disabled={nationwide} />
-          </label>
-          <label className="club-checkbox-row club-checkbox-row-compact">
-            <input
-              type="checkbox"
-              name="nationwide"
-              value="true"
-              checked={nationwide}
-              onChange={(event) => setNationwide(event.target.checked)}
-            />
-            <span>Nationwide</span>
-          </label>
-        </div>
+        <label>
+          <span>City</span>
+          <input name="city" placeholder="Charlotte" required={!nationwide} disabled={nationwide} />
+        </label>
         <label>
           <span>State</span>
           <input name="state" placeholder="NC" maxLength={2} required={!nationwide} disabled={nationwide} />
         </label>
       </div>
+      <label className="club-checkbox-row club-checkbox-row-compact">
+        <input
+          type="checkbox"
+          name="nationwide"
+          value="true"
+          checked={nationwide}
+          onChange={(event) => setNationwide(event.target.checked)}
+        />
+        <span>Nationwide</span>
+      </label>
     </div>
   );
 }
